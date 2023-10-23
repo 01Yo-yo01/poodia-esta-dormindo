c1921cd9b4dc3864c5e17170589c77079bdd2a3e
yo-yo <107567806+01Yo-yo01@users.noreply.github.com>
2023-10-23 01:22:44 -0300

    Delete sla directory

--- sla/slaa.js
+++
@@ -1,9 +0,0 @@
-var audio = document.getElementById('audio');
-var playButton = documente.getElementById('playButton');
-var text = document.getElementById("text")
-
-function olhaOPorco() {
-    audio.play();
-    text.textContent = "É PEGADINHA POWW KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK";
-    playButton.style.display = "none";
-}
